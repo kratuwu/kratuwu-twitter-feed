09c7fffd950c86ceeecbc32fb1b555a81e8e157e
kratuwu <kratuwu@gmail.com>
2022-08-20 00:54:48 +0700

    feat: add google analytic

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,9 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme={"light"}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   );
