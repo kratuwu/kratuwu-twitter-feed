01963acef9275d9bc30d48580864af2eeb02f90a
kratuwu <kratuwu@gmail.com>
2022-08-20 21:10:22 +0700

    fix: fixed random button style

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -25,15 +25,17 @@ const Home: NextPage = () => {
             #NICETOMEETUP
           </h4>
           {isRandom || (
-            <button
-              className="text-2xl text-neutral-700 font-bold py-2 px-4 border border-2 border-r-neutral-500 border-b-neutral-500 p-1 bg-white"
-              onClick={() => setIsRandom(true)}
-            >
-              Random
-            </button>
+            <div className="px-10 sm:w-full ">
+              <button
+                className="text-2xl text-neutral-600 py-2 px-4 border border-2 border-r-neutral-500 border-b-neutral-500 p-1 bg-yellow-200 md:absolute top-0 right-0 w-full md:w-auto md:m-10"
+                onClick={() => setIsRandom(true)}
+              >
+                Random
+              </button>
+            </div>
           )}
         </header>
-        <ReadDataFromCloudFirestore isRandom={isRandom}/>
+        <ReadDataFromCloudFirestore isRandom={isRandom} />
       </main>
 
       <footer></footer>
